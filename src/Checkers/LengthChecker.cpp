9f2cea69d3453806fadd82e5a5654d16c7b6b632
dgelashvili <dgela21@freeuni.edu.ge>
2024-09-02 18:22:41 +0400

    Revert "Login Server Side almost done and tested"
    This reverts commit 82e9cd69c43ac41f7ac3ff2cab444de7a282ab7b.

--- src/Checkers/LengthChecker.cpp
+++
@@ -1,8 +0,0 @@
-#include "LengthChecker.h"
-
-std::string LengthChecker::check(const std::string& password) const {
-    if (password.length() < minLength) {
-        return "Password must be at least " + std::to_string(minLength) + " characters long.";
-    }
-    return PasswordChecker::check(password);
-}
--- src/Checkers/UpperCaseChecker.cpp
+++
@@ -1,9 +0,0 @@
-#include "UpperCaseChecker.h"
-#include <algorithm>
-
-std::string UpperCaseChecker::check(const std::string& password) const {
-    if (!std::ranges::any_of(password, ::isupper)) {
-        return "Password must contain upper-case letter.";
-    }
-    return PasswordChecker::check(password);
-}
